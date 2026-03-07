ffbc005ad5e7a776bac56e5c78301ffedc05b9b4
Evgeniy Korzunov <korzunov.e.v@gmail.com>
2026-03-07 19:21:29 +0500

    fix 2

--- admin/admin_app.py
+++ admin/admin_app.py
@@ -23,7 +23,7 @@ from src.db.models import (
     Product,
     Order,
     Payment,
-    UserEntitlement,
+    UserEntitlement, ProductItem,
 )
 
 
@@ -57,6 +57,7 @@ def create_admin_app():
     admin.add_view(DefaultView(Image, session, category="Content"))
     admin.add_view(DefaultView(TextItem, session, category="Content"))
     admin.add_view(DefaultView(Product, session, category="Acquiring"))
+    admin.add_view(DefaultView(ProductItem, session, category="Acquiring"))
     admin.add_view(DefaultView(Order, session, category="Acquiring"))
     admin.add_view(DefaultView(Payment, session, category="Acquiring"))
     admin.add_view(DefaultView(UserEntitlement, session, category="Acquiring"))
